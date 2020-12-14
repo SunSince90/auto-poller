00edc53f42b9392c669a24ad741200b97e3a154e
Elis Lulja <elulja@cisco.com>
2020-12-14 20:21:11 +0100

    Implement shouldGo in pollInfo
    `shouldGo` returns true if the polling should be done for the page
    
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- poll_info.go
+++ poll_info.go
@@ -10,9 +10,35 @@ type pollInfo struct {
 	remaining uint
 	f         CallBack
 
-	sync.Mutex
+	lock sync.Mutex
 }
 
 // CallBack is a pointer to a function that must be executed after
 // an http call is performed
 type CallBack func(*http.Response)
+
+func newPollInfo(page *WebsitePage, f CallBack) *pollInfo {
+	return &pollInfo{
+		WebsitePage: page,
+		f:           f,
+		remaining:   nextRandom(10, 60),
+	}
+}
+
+func (i *pollInfo) shouldGo() bool {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+
+	if i.remaining == 0 {
+		if i.WebsitePage.PollSettings.Type == FixedPolling {
+			i.remaining = *i.WebsitePage.Frequency
+		} else {
+			i.remaining = nextRandom(*i.WebsitePage.RandMin, *i.WebsitePage.RandMax)
+		}
+
+		return true
+	}
+
+	i.remaining--
+	return false
+}
