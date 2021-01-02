e1ad6b04aa1adcc9c5c2cceec6713b4e5c47f40c
Elis Lulja <elulja@cisco.com>
2021-01-02 15:17:57 +0100

    Create Poller interface
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SunSince90/website-poller
 
 go 1.15
 
-require github.com/sirupsen/logrus v1.7.0
+require (
+	github.com/rs/zerolog v1.20.0
+	github.com/sirupsen/logrus v1.7.0
+)
--- poller.go
+++ poller.go
@@ -1,152 +1,49 @@
-package autopoller
+package websitepoller
 
 import (
 	"context"
-	"net/http"
-	"strings"
-	"sync"
-	"time"
+	"os"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/rs/zerolog"
 )
 
-// Poller performs continous polling to websites
-type Poller interface {
-	Start(ctx context.Context, exitChan chan struct{})
-	AddUserAgents(uas []string)
-	AddPage(page *WebsitePage, f CallBack)
-	RemovePage(id string)
+var (
+	log zerolog.Logger
+)
+
+func init() {
+	output := zerolog.ConsoleWriter{Out: os.Stdout}
+	log = zerolog.New(output).With().Timestamp().Logger()
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 }
 
-type poll struct {
-	pages      map[string]*pollInfo
-	globalUAs  []string
-	httpClient *http.Client
-	lock       sync.Mutex
+// Poller is in charge of polling a website and providing results to a
+// function that will handle the result
+type Poller interface {
+	// Start polling
+	Start(ctx context.Context, now bool)
+	// SetHandlerFunc sets the function that will be called when a poll has
+	// finished
+	SetHandlerFunc(HandlerFunc)
+}
+
+type pagePoller struct {
+	// TODO: implement me
 }
 
 // New returns a new instance of the poller
-func New(timeout int, redir bool) Poller {
-	to := time.Duration(timeout) * time.Second
-	httpClient := &http.Client{
-		Timeout: to,
-	}
-
-	if !redir {
-		httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		}
-	}
-
-	return &poll{
-		pages:      map[string]*pollInfo{},
-		globalUAs:  []string{},
-		httpClient: httpClient,
-	}
-}
-
-// AddUserAgents adds user agents for all pages, except those that
-// set their owns
-func (p *poll) AddUserAgents(uas []string) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	p.globalUAs = append(p.globalUAs, uas...)
+func New(p *Page) (Poller, error) {
+	// TODO: implement me
+	return nil, nil
 }
 
 // Start polling
-func (p *poll) Start(ctx context.Context, exitChan chan struct{}) {
-	l := log.WithField("func", "poll.Start")
-	l.Info("starting...")
-
-	ticker := time.NewTicker(time.Second)
-	for {
-		select {
-		case <-ticker.C:
-			p.check(ctx)
-		case <-ctx.Done():
-			l.Info("stop requested")
-			ticker.Stop()
-			close(exitChan)
-			return
-		}
-	}
+func (p *pagePoller) Start(ctx context.Context, now bool) {
+	// TODO: implement me
 }
 
-// AddPage adds a new website to poll
-func (p *poll) AddPage(page *WebsitePage, f CallBack) {
-	l := log.WithFields(log.Fields{"func": "poll.AddPage", "id": page.ID})
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	p.pages[page.ID] = newPollInfo(page, f)
-	l.WithField("polling-in", p.pages[page.ID].remaining).Info("added")
-}
-
-// RemovePage removes website page
-func (p *poll) RemovePage(id string) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	delete(p.pages, id)
-}
-
-func (p *poll) check(ctx context.Context) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
-	for _, page := range p.pages {
-		if page.shouldGo() {
-			go p.do(ctx, page)
-		}
-	}
-}
-
-func (p *poll) do(ctx context.Context, pi *pollInfo) {
-	l := log.WithFields(log.Fields{"func": "poll.do", "id": pi.ID})
-	uas := p.globalUAs
-	ua := ""
-
-	// Get the user agent
-	if len(pi.UserAgents) > 0 {
-		if len(pi.UserAgents) == 1 {
-			ua = pi.UserAgents[0]
-		}
-		uas = pi.UserAgents
-	} else {
-		if len(p.globalUAs) > 0 {
-			n := nextRandom(0, len(uas)-1)
-			ua = uas[n]
-		}
-	}
-
-	l.Debug("using user agent", ua)
-
-	resp, err := p.doRequest(ctx, pi.URL, ua)
-	if err != nil {
-		l.WithError(err).Error("error while doing request")
-	}
-
-	// Pass this to the callback
-	go pi.f(pi.WebsitePage, resp, err)
-}
-
-func (p *poll) doRequest(ctx context.Context, url, ua string) (resp *http.Response, err error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return
-	}
-
-	if len(ua) > 0 {
-		req.Header.Add("User-Agent", ua)
-	}
-
-	resp, err = p.httpClient.Do(req)
-	if err != nil {
-		if strings.Contains(err.Error(), context.DeadlineExceeded.Error()) {
-			err = context.DeadlineExceeded
-		}
-	}
-
-	return
+// SetHandlerFunc sets the function that will be called when a poll has
+// finished
+func (p *pagePoller) SetHandlerFunc(f HandlerFunc) {
+	// TODO: implement me
 }
