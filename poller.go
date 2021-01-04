9a811130e87c4cd7b6eb1543591d44e8dfea6e77
Elis Lulja <elulja@cisco.com>
2021-01-04 21:21:23 +0100

    Move interface to a separate file
    Signed-off-by: Elis Lulja <elulja@cisco.com>

--- poller.go
+++ poller.go
@@ -30,21 +30,6 @@ func init() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 }
 
-// Poller is in charge of polling a website and providing results to a
-// function that will handle the result
-type Poller interface {
-	// Start polling
-	Start(ctx context.Context, now bool)
-	// SetHandlerFunc sets the function that will be called when a poll has
-	// finished
-	SetHandlerFunc(HandlerFunc)
-	// GetID returns the ID of this poller. If the `Page` struct provided
-	// to `New` contained a non-empty `ID`, then this returns the same ID as
-	// the one contained in there, otherwise it returns a randomly generated
-	// one.
-	GetID() string
-}
-
 type pagePoller struct {
 	id          string
 	httpClient  *http.Client
